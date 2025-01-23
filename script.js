5e3213ce593c9547acf4eeaa7ea1aa5a2c33be42
William <wfchen30@gmail.com>
2025-01-23 17:53:40 -0500

    line animation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    const paths = document.querySelectorAll("path");
+  
+    paths.forEach((path) => {
+      const length = path.getTotalLength();
+      path.style.strokeDasharray = length;
+      path.style.strokeDashoffset = length;
+  
+      setTimeout(() => {
+        path.style.transition = "stroke-dashoffset 1s linear";
+        path.style.strokeDashoffset = "0";
+      }, 2000);
+    });
+  });
+  
